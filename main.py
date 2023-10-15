0f7eddb5017e7b69768d5e10d51717c0e249099c
kevin-pek <76090938+kevin-pek@users.noreply.github.com>
2023-10-15 22:31:34 +0800

    changed to langchain

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
-from haystack.nodes import PreProcessor, PDFToTextConverter, EmbeddingRetriever, TransformersReader
-from haystack.document_stores import InMemoryDocumentStore
-from haystack.pipelines import DocumentSearchPipeline, ExtractiveQAPipeline
+import os
+import sys
+
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
 import gradio as gr
 
-preprocessor = PreProcessor(
-    clean_empty_lines=True,
-    clean_whitespace=True,
-    clean_header_footer=True,
-    split_by="word",
-    split_length=100,
-    split_respect_sentence_boundary=True,
-    split_overlap=3
+# Enable to cache & reuse the model to disk (for repeated queries on the same data)
+PERSIST = False
+
+query = sys.argv[1]
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+if PERSIST and os.path.exists("persist"):
+  print("Reusing index...\n")
+  raw_documents = DirectoryLoader("persist").load()
+  text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+  documents = text_splitter.split_documents(raw_documents)
+  vectorstore = FAISS.from_documents(documents, embedding=embeddings)
+  from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+else:
+  loader = TextLoader('data.txt')
+  # This code can also import folders, including various filetypes like PDFs using the DirectoryLoader.
+  # loader = DirectoryLoader(".", glob="*.txt")
+  if PERSIST:
+    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+  else:
+    index = VectorstoreIndexCreator().from_loaders([loader])
+
+chain = RetrievalQA.from_chain_type(
+  llm=,
+  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 5}),
 )
-document_store = InMemoryDocumentStore(embedding_dim=384)
-reader = TransformersReader("sentence-transformers/all-MiniLM-L6-v2")
-retriever = EmbeddingRetriever(document_store=document_store, embedding_model="sentence-transformers/all-MiniLM-L6-v2")
-pipeline = ExtractiveQAPipeline(reader, retriever)
-converter = PDFToTextConverter(remove_numeric_tables=True)
-
-def print_answers(results):
-    fields = ["answer", "score"] # "context"
-    answers = results["answers"]
-    filtered_answers = []
-    for ans in answers:
-        filtered_ans = {
-            field: getattr(ans, field) for field in fields if getattr(ans, field) is not None
-        }
-        filtered_answers.append(filtered_ans)
-    return filtered_answers
-
-def write_pdf(pdf_file):
-    document = converter.convert(file_path=pdf_file.name, meta=None)[0]
-    preprocessed_docs = preprocessor.process(document)
-    document_store.write_documents(preprocessed_docs)
-    document_store.update_embeddings(retriever)
-
-def predict(question, pdf_file):
-    write_pdf(pdf_file)
-    result = pipeline.run(query=question, params={"Retriever": { "top_k": 2 }})
-    answers = print_answers(result)
-    return answers
+print(chain.run(query))
 
 interface = gr.Interface(
     fn=predict,
